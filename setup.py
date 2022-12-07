b836f799087a1b3b1cfec5e088efa39fbda9b871
Aleksander Peczot <Aleksander.Peczot@teaminternational.com>
2022-12-07 19:50:40 +0100

    adjustments

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from distutils.core import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
-  name = 'sandpy',         # How you named your package folder (MyLib)
-  packages = ['sandpy'],   # Chose the same as "name"
+  name = 'sandpy-client',         # How you named your package folder (MyLib)
+  packages = ['sandpy-client'],   # Chose the same as "name"
   version = '1.0.0',      # Start with a small number and increase it with every change you make
   license='apache-2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Client for sandpy cloud',   # Give a short description about your library
   author = 'Aleksander Peczot',                   # Type in your name
   author_email = 'peczot.a@gmail.com',      # Type in your E-Mail
-  url = 'sandpy.com',   # Provide either the link to your github or to your website
+  url = 'https://sandpy.com',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/ap74062/sandpy/archive/refs/tags/1.0.tar.gz',    # I explain this later on
   keywords = ['CLOUD', 'COMPUTING', 'CLOUDCOMPUTING'],   # Keywords that define your package best
   install_requires=[],
